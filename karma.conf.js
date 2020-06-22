ee6d0ea8107343746b311db1d61001d7beef8197
ValievMA <maratv85@gmail.com>
2020-06-22 12:48:00 +0300

    Edit karma config

--- karma.conf.js
+++ karma.conf.js
@@ -3,67 +3,71 @@
 
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
+    frameworks: ['jasmine', 'jasmine-jquery'],
+    plugins: ['@metahub/karma-jasmine-jquery', 'karma-*'],
 
     // list of files / patterns to load in the browser
     files: [
-      'spec.ts'
+      'node_modules/jquery/dist/jquery.js',
+      'src/plugin/**/*.ts',
+      'src/test/**/*.spec.ts'
     ],
-
-
-    // list of files / patterns to exclude
-    exclude: [
-    ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/plugin/**/*.ts': ['karma-typescript', 'sourcemap', 'coverage'],
+      'src/test/**/*.ts': ['karma-typescript',  'sourcemap'],
+      'karma.conf.js': ['karma-typescript',  'sourcemap']
     },
 
+    karmaTypescriptConfig: {
+      compilerOptions: {
+        noImplicitAny: true,
+        noImplicitReturns: true,
+        noImplicitThis: true,
+        allowSyntheticDefaultImports: true,
+        lib: ['DOM', 'ES5', 'ScriptHost', 'ES2015.Core', 'ES2015.Iterable']
+      }
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec', 'coverage-istanbul'],
 
+    specReporter: {
+      maxLogLines: 5, 
+      suppressErrorSummary: true, 
+      suppressFailed: false, 
+      suppressPassed: false, 
+      suppressSkipped: true, 
+      showSpecTiming: true 
+    },
 
-    // web server port
+    coverageIstanbulReporter: {
+      reports: ['html', 'text-summary'],
+      dir: './test/coverage', 
+      fixWebpackSourcePaths: true,
+      query: {
+        esModules: true
+      }
+    },
+
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Opera', 'Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    browsers: ['Chrome'],
     singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity
   })
 }
