d63077022eba2b8302d6e55e9f69ceadcf7b55a2
ValievMA <maratv85@gmail.com>
2020-06-25 09:22:18 +0300

    Add settings to slider

---
+++ src/plugin/model/RSliderSettings.ts
@@ -0,0 +1,11 @@
+export interface RSliderSettings {
+  step?: number;
+  isVertical?: boolean;
+  isRange?: boolean;
+  min?: number;
+  max?: number;   
+  from?: number;
+  to?: number;
+  hasTip?: boolean;
+  hasScale?: boolean;
+}
