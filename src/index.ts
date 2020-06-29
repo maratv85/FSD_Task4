3867ea32954a85bdb52a556ea0cc663b617d8a2e
ValievMA <maratv85@gmail.com>
2020-06-29 14:24:16 +0300

    Update fonts

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ function importAll(resolve: any) {
 }
 importAll(require.context('./style', true, /\.(css|scss)$/));
 importAll(require.context('./static/favicon/', true, /\.(svg|png|ico|xml|json)$/),);
+importAll(require.context('./static/fonts/', true, /\.(eot|svg|ttf|woff|woff2)$/),);
 importAll(require.context('./', true, /^(?!.*(?:spec.ts$)).*\.ts|.scss$/));
