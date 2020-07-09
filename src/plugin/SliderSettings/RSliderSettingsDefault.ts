30b2bbeb27c02b0d1788809f73efcf918fcdc737
ValievMA <maratv85@gmail.com>
2020-07-09 11:06:48 +0300

    Fix slider settings

--- src/plugin/SliderSettings/RSliderSettingsDefault.ts
+++ src/plugin/SliderSettings/RSliderSettingsDefault.ts
@@ -1,15 +1,15 @@
-import {IRSliderSettings} from './IRSliderSettings';
+import {IRSliderSettings} from './RSliderSettings';
 
 const RSliderSettingsDefault: IRSliderSettings = {
-  step: 1;
-  isVertical: false;
-  isRange: false;
-  min: 0;
-  max: 100;   
-  from: 0;
-  to: null;
-  hasTip: false;
-  hasScale: false;
+  step: 1,
+  isVertical: false,
+  isRange: false,
+  min: 0,
+  max: 100,  
+  from: 0,
+  to: null,
+  hasTip: false,
+  hasScale: false,
 }
 
 export default RSliderSettingsDefault
