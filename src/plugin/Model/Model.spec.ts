7ea9295ca49c856c28b59cac7d16bb17201e9ce4
ValievMA <maratv85@gmail.com>
2020-07-28 00:13:06 +0300

    Update Model.spec.ts

---
+++ src/plugin/Model/Model.spec.ts
@@ -0,0 +1,14 @@
+import { Model } from './Model';
+
+describe('Model. Test set values', () => {
+  const model = new Model({
+    step: 5,
+    min: 0,
+    max: 100,
+    from: 20,
+    to: 70,
+    isRange: true
+  })
+
+  
+})
